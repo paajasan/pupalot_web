ca50e67d3019a9675a17a76fc3ff15b335c6ab2a
Santeri Paajanen <paajanensanteri@gmail.com>
2018-09-16 02:25:46 +0300

    Reorganised the files so that the web page root folder does not have any of the code

---
+++ codes/db_classes/wine.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+from django.db import models
+
+
+class Wine(models.Model):
+    pass
---
+++ web_root/pipalot_web.wsgi
@@ -0,0 +1,4 @@
+import sys
+sys.path.insert(0, '/home/pi/pipalot_web')
+
+from app import app as application
