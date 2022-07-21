e87bca4d33f0d812153e9cb1b0e2fbdf24b0f5bc
Santeri Paajanen <paajanensanteri@gmail.com>
2022-07-21 22:31:24 +0300

    Fixed typo in /dev/null in install.sh
    Also swapped conditional in diff check

--- codes/install.sh
+++ codes/install.sh
@@ -16,7 +16,7 @@ restart_apache=false
 # check both conf files
 for f in pipalot_web.conf pipalot_web-le-ssl.conf; do
     # Check for change
-    if diff /etc/apache/sites-enabled/$f codes/$f  &>/den/null; then
+    if ! diff /etc/apache/sites-enabled/$f codes/$f  &>/dev/null; then
         # If changed, copy to sites-available
         sudo cp codes/$f /etc/apache/sites-available/
         # Remake link to sites-enabled if nonexistent
